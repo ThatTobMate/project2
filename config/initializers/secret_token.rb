5d8c7a59437ac8a9339d41c9c7b4136ff6646b3e
cerico <garethrobertlee@gmail.com>
2014-08-15 14:37:19 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RssFeederApp::Application.config.secret_token = '903c7e1c1ff0eaf938fb22967bd4ec6fb7417ec72f77015871e6df24bb077245232070e6faa8855cc6d06f9d033a45e79b31cbc88c22e96da25be78de23cccdc'
