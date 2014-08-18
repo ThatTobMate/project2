d2e7095fb5a7b48bbd74b8d6350e454b185dac78
cerico <garethrobertlee@gmail.com>
2014-08-18 18:52:59 +0100

    relationship between articles and subscriptions now improved

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,38 @@
 class Article < ActiveRecord::Base
-  attr_accessible :entry_id, :image, :published, :summary, :title, :url
+  attr_accessible :entry_id, :image, :published, :summary, :title, :url, :subscription_id
+
+  def self.update_from_feed(feed_url,id)
+    feed = Feedjira::Feed.fetch_and_parse(feed_url)
+    @subscription_id = id
+    add_entries(feed.entries)
+  end
+  
+  # def self.update_from_feed_continuously(feed_url, delay_interval = 2.minutes)
+  #   feed = Feedjira::Feed.fetch_and_parse(feed_url)
+  #   add_entries(feed.entries)
+  #   loop do
+  #     sleep delay_interval
+  #     feed = Feedjira::Feed.update(feed)
+  #     add_entries(feed.new_entries) if feed.updated?
+  #   end
+  # end
+  
+  private
+  
+  def self.add_entries(entries)
+    entries.each do |entry|
+      unless exists? :entry_id => entry.id
+        create!(
+          :title         => entry.title,
+          :summary      => entry.summary,
+          :url          => entry.url,
+          :published => entry.published,
+          :entry_id         => entry.id,
+          :subscription_id  => @subscription_id
+        )
+      end
+    end
+  end
+
+  belongs_to :subscription
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,4 +3,5 @@ class Subscription < ActiveRecord::Base
 
 
   has_and_belongs_to_many :users
+  has_many :articles
 end
