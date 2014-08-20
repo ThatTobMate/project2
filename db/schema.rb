be7fca0ac8d836a5d4018b262fb63d203964c87d
Tobias Hale <thale.android@gmail.com>
2014-08-20 17:37:03 +0100

    styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140819182103) do
+ActiveRecord::Schema.define(:version => 20140820160556) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(:version => 20140819182103) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
     t.integer  "category_id"
+    t.string   "image"
+    t.text     "description"
   end
 
   create_table "subscriptions", :force => true do |t|
