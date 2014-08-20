b44e9bb2b8cfe70d3f27182f149a6c7e657aa367
David Rees <davidarees@outlook.com>
2014-08-20 16:35:21 +0100

    side bar initial changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authenticate_user!, except: :index
+  before_filter :get_subs
+  # GET /subscriptions
+  # GET /subscriptions.json
+  def get_subs
+
+if current_user
+    @subscriptions = Subscription.where(user_id: current_user.id)
+    @subscriptions_list = @subscriptions.group_by { |t| t.category.name }
+
+else  
+    @subscriptions = []
+    @subscriptions_list = []
+
+  end
+    
+  end
 end
