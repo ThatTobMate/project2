bb56f05ce5d7a7c41a6ed6b54def7fa1833bb150
cerico <garethrobertlee@gmail.com>
2014-08-20 18:30:47 +0100

    bookmarks added to bar

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20140820160556) do
-
 
   create_table "articles", :force => true do |t|
     t.string   "title"
