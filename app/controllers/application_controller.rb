0822a39a2ed186bca38a556181be5585aa4e6677
Tobias Hale <thale.android@gmail.com>
2014-08-20 11:28:57 +0100

    ransack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def set_ransack_form_variables
+    q = params[:q]
+      @feeds = Feed.search(name_cont: q).result
+      @categories = Category.search(name_cont: q).result
+      @articles = Article.search(name_cont: q).result
+  end
+
 end
