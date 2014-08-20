f082090a4bd5775c89c15daf6c3ace5894feeb7e
cerico <garethrobertlee@gmail.com>
2014-08-20 18:50:04 +0100

    bookmark removal, start

--- app/helpers/articles_users_helper.rb
+++ app/helpers/articles_users_helper.rb
@@ -1,2 +1,13 @@
 module ArticlesUsersHelper
+  def remove_bookmark
+    @article_user = ArticlesUser.find(params[:id])
+    @article_user.update_attributes(is_bookmarked:false)
+    @article_user.save
+
+    respond_to do |format|
+      format.html { redirect_to articles_users_url }
+      format.json { head :no_content }
+    end
+  end
+
 end
