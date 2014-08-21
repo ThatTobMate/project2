a3b3a7bdaa19d72e33cf740430a6620c8ebe3200
David Rees <davidarees@outlook.com>
2014-08-21 19:58:46 +0100

    added in the button work in subscription page

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,2 +1,10 @@
 module SubscriptionsHelper
+  def is_user_subscribed?(feed)
+    feed.subscriptions.map{|subs| subs.user_id}.include?(current_user.id)
+  end
+
+  def get_subscription_id(feed)
+    feed.subscriptions.where(user_id: current_user.id).first.id
+    
+  end
 end
